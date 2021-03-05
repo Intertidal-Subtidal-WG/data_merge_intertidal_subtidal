fc0eea67dbad91ab16665ad3d7e5bfa4a55a7713
jebyrnes <jarrett.byrnes@umb.edu>
2021-03-04 23:50:08 -0500

    The first attempt at long data combination.... it is done.

--- scripts/combine_both_abundance_data.R
+++ scripts/combine_both_abundance_data.R
@@ -2,5 +2,42 @@
 #' @description Combine Intertidal and Subtidal Datasets of Abundances
 #' @author Jarrett Byrnes
 #'-----------------------------------------------------------------------
+library(readr)
+library(dplyr)
 
 setwd(here::here())
+
+subtidal <- readRDS("tidy_data/combined_subtidal_abundance.RDS")
+intertidal <- readRDS("tidy_data/combined_intertidal_abundance.RDS")
+
+#aggregate for combination
+
+#subtidal to transect level for each protocol
+subtidal <- subtidal %>%
+  group_by(YEAR, SITE, TRANSECT, INTERTIDAL_TRANSECT, SP_CODE, AREA,
+           across(GROUP:PROTOCOL)) %>%
+  summarize(VALUE = mean(VALUE)) %>%
+  ungroup() %>%
+  select(YEAR, SITE, TRANSECT, INTERTIDAL_TRANSECT, 
+         PROTOCOL, SPECIES, SIZE, MEASURE, VALUE, AREA, everything()) %>%
+  rename(ORGANISM = SPECIES)
+
+
+#intertidal to tide height:transect level for each protocol
+intertidal <- intertidal %>%
+  group_by(YEAR, SITE, TRANSECT, INTERTIDAL_TRANSECT, LEVEL, 
+           ORGANISM, ORGANISM_TYPE, AREA,
+           across(MEASURE:GENUS)) %>%
+  summarize(VALUE = mean(VALUE)) %>%
+  ungroup() %>%
+  select(YEAR, SITE, TRANSECT, INTERTIDAL_TRANSECT, 
+         PROTOCOL, ORGANISM, ORGANISM_TYPE, MEASURE, VALUE, AREA, everything())
+
+
+#join the datasets
+combined_data <- bind_rows(subtidal, intertidal) %>%
+  filter(YEAR>2013) #so it's constrained to years that both were conducted
+
+
+write_csv(combined_data, "tidy_data/combined_all_abundance_data.csv")
+saveRDS(combined_data, "tidy_data/combined_all_abundance_data.RDS")
